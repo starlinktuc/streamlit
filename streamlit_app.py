7cfd10b5038dffb6a1dc941f9a9e1d5fdf5f0a4f
starlinktuc <154489723+starlinktuc@users.noreply.github.com>
2023-12-22 22:02:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 from langchain.llms import OpenAI
 
-st.title("🦜🔗 OpenAI con Langchain")
+st.title("🦜🔗 Langchain Quickstart App")
 
 with st.sidebar:
-    openai_api_key = st.text_input("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
-", type="password")
+    openai_api_key = st.text_input("OpenAI API Key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
 
 
@@ -15,7 +14,7 @@ def generate_response(input_text):
 
 
 with st.form("my_form"):
-    text = st.text_area("Enter text:", "cuántas ciudades tiene la provincia de tucuman?")
+    text = st.text_area("Enter text:", "What are 3 key advice for learning how to code?")
     submitted = st.form_submit_button("Submit")
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
